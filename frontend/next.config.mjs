c1cb880f6ab1be0bfe7f9f572f82e24adba38c14
Vimal Kumar M <vimal@iimcal.ac.in>
2025-08-12 14:15:42 +0530

    Update next.config.mjs
    Creating Proxy API

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,1 +1,8 @@
-export default { reactStrictMode: true };
+const API = process.env.NEXT_PUBLIC_API_BASE || "https://trading-platform-mv-1.onrender.com";
+export default {
+  reactStrictMode: true,
+  async rewrites() {
+    return [{ source: "/api/:path*", destination: `${API}/api/:path*` }];
+  },
+};
+
